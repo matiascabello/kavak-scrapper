388e984701bcf3e0cf6e3243640ad159cb43ae2c
matiascabello <matias.cabello@gmail.com>
2022-10-29 20:38:42 -0300

    added cron job

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import * as cron from 'node-cron';
 
 import { scrapTask } from './scrapper.js';
 
@@ -11,4 +12,7 @@ app.listen(process.env.PORT || 8000, () => console.log('App running'));
 
 const url = 'https://www.kavak.com/ar/autos-Argo/ciudad-Buenos_Aires/autos-usados';
 
-scrapTask(url);
+cron.schedule('0 0 */12 * *', () => {
+    scrapTask(url);
+})
+
